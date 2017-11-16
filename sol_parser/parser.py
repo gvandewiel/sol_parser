86b9d243281b65d4ed9ef679d181a0e75c65603c
Gijs van de Wiel <gvandewiel@gmail.com>
2017-11-16 07:31:49 +0100

    Added functions to calculate turnover date and age Added function to filter out scout to be transferred to next gropu

--- sol_parser/parser.py
+++ sol_parser/parser.py
@@ -1,4 +1,39 @@
-class parser():
+from datetime import date
+from datetime import *
+from dateutil.relativedelta import relativedelta
+
+
+class Parser():
     def __init__(self, csvfile):
-        with open(csvfile) as csvfile:  
-            self.reader = csv.DictReader(csvfile)
+        self.objLeden = dict()
+        self.chk_date = turnover_date()
+
+        with open(csvfile) as csvfile:
+            try:
+                self.reader = csv.DictReader(csvfile)
+                for row in reader:
+                    row['leeftijd'] = calculate_age(row['Lid geboortedatum'], date.today())
+                    row['chk_leeftijd'] = calculate_age(row['Lid geboortedatum'], self.chk_date)
+                    objLeden[row['Lidnummer']] = row
+            finally:
+                csvfile.close()
+
+    def turnover_date():
+        # setup date for checking age
+        turnover_date = date.today()
+        if turnover_date.month >= 9 and turnover_date.day >= 1:
+            turnover_date = turnover_date + relativedelta(day=1, month=9, years=1)
+        else:
+            turnover_date = turnover_date + relativedelta(day=1, month=9)
+
+        return turnover_date
+
+    def find_rsa():
+        rsa = dict()
+        new_rsa = 0
+
+        for y in objLeden:
+            if objLeden[y]['chk_leeftijd'] >= 14 and objLeden[y]['Functie'] == "jeugdlid *":
+                new_rsa = new_rsa+1
+                rsa[objLeden[y]['Lidnummer']] = objLeden[y]
+        return rsa, new_rsa
