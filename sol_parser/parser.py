99fe422a8fa27a8f8690726b02bb97c0c89cfa07
Gijs van de Wiel <gvandewiel@gmail.com>
2017-11-16 07:42:12 +0100

    Added docstring to all functions

--- sol_parser/parser.py
+++ sol_parser/parser.py
@@ -1,10 +1,26 @@
+"""Summary
+"""
 from datetime import date
 from datetime import *
 from dateutil.relativedelta import relativedelta
 
 
 class Parser():
+
+    """Summary
+
+    Attributes:
+        chk_date (TYPE): Description
+        objLeden (TYPE): Description
+        reader (TYPE): Description
+    """
+
     def __init__(self, csvfile):
+        """Summary
+
+        Args:
+            csvfile (TYPE): Description
+        """
         self.objLeden = dict()
         self.chk_date = turnover_date()
 
@@ -12,23 +28,36 @@ class Parser():
             try:
                 self.reader = csv.DictReader(csvfile)
                 for row in reader:
-                    row['leeftijd'] = calculate_age(row['Lid geboortedatum'], date.today())
-                    row['chk_leeftijd'] = calculate_age(row['Lid geboortedatum'], self.chk_date)
+                    row['leeftijd'] = calculate_age(
+                        row['Lid geboortedatum'], date.today())
+                    row['chk_leeftijd'] = calculate_age(
+                        row['Lid geboortedatum'], self.chk_date)
                     objLeden[row['Lidnummer']] = row
             finally:
                 csvfile.close()
 
     def turnover_date():
+        """Summary
+
+        Returns:
+            TYPE: Description
+        """
         # setup date for checking age
         turnover_date = date.today()
         if turnover_date.month >= 9 and turnover_date.day >= 1:
-            turnover_date = turnover_date + relativedelta(day=1, month=9, years=1)
+            turnover_date = turnover_date + \
+                relativedelta(day=1, month=9, years=1)
         else:
             turnover_date = turnover_date + relativedelta(day=1, month=9)
 
         return turnover_date
 
     def find_rsa():
+        """Summary
+
+        Returns:
+            TYPE: Description
+        """
         rsa = dict()
         new_rsa = 0
 
