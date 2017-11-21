b919d2910a04d23bf3e6758acd871c401bace262
Gijs van de Wiel <gvandewiel@gmail.com>
2017-11-21 07:42:23 +0100

    Finished reporting template Added summary page in HTML

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from sol_parser.parser import Parser
 from contributie import contr_list
 from pprint import pprint
 from fpdf import FPDF
+import os
 
 class PDF(FPDF):
     def header(self):
@@ -68,21 +69,28 @@ def html_start(html_file):
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title></title>
         <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
-        <link rel="stylesheet" type="text/css" href="style.css">
         <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
         <script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-        <script src="scroller.js"></script>
+        <script>
+            $('*[data-href]').on("click",function(){
+              window.location = $(this).data('href');
+              return false;
+            });
+            $("td > a").on("click",function(e){
+              e.stopPropagation();
+            });
+        </script>
     </head>
     <body>
         <div class="container">
             <table class="table-striped">
                 <thead>
                     <tr>
-                        <th>Nummer</th>
-                        <th>Adres</th>
-                        <th>Achternaam</th>
-                        <th>Contributie</th>
-                        <th>Email</th>
+                        <th class="col-xs-2">Notanummer</th>
+                        <th class="col-xs-2">Adres</th>
+                        <th class="col-xs-3">Achternaam</th>
+                        <th class="col-xs-1">Contributie</th>
+                        <th class="col-xs-2">Email</th>
                    </tr>
                </thead>
                <tbody>
@@ -105,6 +113,11 @@ if __name__ == '__main__':
     """Create adres dictionary which contains all members grouped on adres.
     For each adres a contribution letter is generated
     """
+
+    # Create output directory
+    output_dir = 'pdf'
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
     html_file = open("output.html", "w")
     html_start(html_file)
@@ -169,7 +182,15 @@ if __name__ == '__main__':
             pdf.cell(w=100, h=6, txt='', ln=0)
             pdf.cell(w=40, h=6, txt='Totaal', ln=0, align='L')
             pdf.cell(w=0, h=6, txt='{}'.format(t_contr), ln=1, align='R')
+
             print(lid.Lid_adres)
-            html_file.write("<tr><td>20172018{}</td><td>{}</td><td>{}</td>".format(iNotanumber, lid.Lid_adres, lid.Lid_achternaam, s_contr))
-            pdf.output('20172018{}.pdf'.format(iNotanumber), 'F')
+
+            html_file.write('<tr>')
+            html_file.write('   <td class="col-xs-2">20172018{}</td>'.format(iNotanumber))
+            html_file.write('   <td class="col-xs-2">{}</td>'.format(lid.Lid_adres))
+            html_file.write('   <td class="col-xs-3">{}</td>'.format(lid.Lid_achternaam))
+            html_file.write('   <td class="col-xs-1 text-right">{}</td>'.format(s_contr))
+            html_file.write('   <td class="col-xs-3">{}</td>'.format(lid.Lid_e_mailadres))
+            html_file.write('</tr>')
+            pdf.output(os.path.join(output_dir, '20172018{} - {}.pdf'.format(iNotanumber, lid.Lid_e_mailadres)), 'F')
     html_end(html_file)
