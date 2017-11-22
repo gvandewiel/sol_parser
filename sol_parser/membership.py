39736385a7f547876163975c5533b474d0830d77
Gijs van de Wiel <gvandewiel@gmail.com>
2017-11-22 14:28:37 +0100

    Update membership.py
    Started list class

---
+++ sol_parser/membership.py
@@ -0,0 +1,10 @@
+class list(object):
+    """Create printable list of scouts.
+    Uses the parsed SOL output as input
+    And creates a list with all relevant data
+    """
+    def __init__(self, parser):
+        pass
+
+    def generate_list():
+        pass
