153b5b17e76e42569bd59cf01139bbe8a86c80bb
Gijs van de Wiel <gvandewiel@gmail.com>
2017-11-24 18:17:09 +0100

    Started with list method

--- sol_parser/membership.py
+++ sol_parser/membership.py
@@ -4,7 +4,20 @@ class list(object):
     And creates a list with all relevant data
     """
     def __init__(self, parser):
-        pass
+        self.parser = parser
 
-    def generate_list():
-        pass
+    def generate_list(self):
+        scout_list = self.parser
+        out_list = open(list.html, “w”)
+        out_list.write(“<table>”)
+        out_list.write(“<tr>”)
+        for key in scout_list[0].keys():
+            out_list.write(“<th>{}</th>”.format(key))
+        out_list.write(“</tr>”)
+
+        for scout in scout_list:
+            out_list.write(“<tr>”)
+            for ,v in scout.items():
+                out_list.write(“<td>{}</td>”.format(v))
+            out_list.write(“</tr>”)
+        out_file.write(“</table>”)
